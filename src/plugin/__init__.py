574bf4b5cabf72d68bf8dca5ae3ada4f6cd6e006
mz01-sts <sts@mz.co.kr>
2025-09-05 18:57:06 +0900

    style: Use relative imports in plugin's __init__.py
    - This commit refactors the import statements in `src/plugin/__init__.py` to use relative imports instead of absolute ones.

--- src/plugin/__init__.py
+++ src/plugin/__init__.py
@@ -1,5 +1,5 @@
-from plugin.manager.cost_manager import CostManager as CostManager
-from plugin.manager.data_source_manager import DataSourceManager as DataSourceManager
-from plugin.manager.job_manager import JobManager as JobManager
+from .manager.cost_manager import CostManager as CostManager
+from .manager.data_source_manager import DataSourceManager as DataSourceManager
+from .manager.job_manager import JobManager as JobManager
 
 __all__ = ["DataSourceManager", "JobManager", "CostManager"]
